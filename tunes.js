8d27b37cb9cbb917e3dc7735cb19eefe60282c17
Thomas McEvoy <tmcevoy213@gmail.com>
2018-02-27 00:11:38 -0500

    troubleshooting server

--- tunes.js
+++ tunes.js
@@ -1,7 +1,7 @@
-// const index = require('./index.html')
-const Koa = import('koa')
+const index = require('./index.html')
+const Koa = require('koa')
 const app = new Koa()
 
-app.use(require('koa-static')('/home/thomas/tunes/build'));
-// app.use(ctx => ctx.body = index)
+// app.use(require('koa-static')('/home/thomas/tunes/build'));
+app.use(ctx => ctx.body = index)
 app.listen(3002)
