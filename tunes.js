a163aacd9a080d89181661ff367c87a600b87528
Thomas McEvoy <tmcevoy213@gmail.com>
2018-02-27 00:19:04 -0500

    clean up server

--- tunes.js
+++ tunes.js
@@ -1,7 +1,5 @@
-// const index = require('./build/index.html')
 const Koa = require('koa')
 const app = new Koa()
 
 app.use(require('koa-static')('/home/thomas/tunes/build'))
-// app.use(ctx => ctx.body = index)
 app.listen(3002)
