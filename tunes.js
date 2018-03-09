362b171bfba7c67a2fd73308f9f51b11cb6aaf4a
Thomas McEvoy <tmcevoy213@gmail.com>
2018-03-09 15:43:14 -0500

    test

--- tunes.js
+++ tunes.js
@@ -2,7 +2,11 @@ const serve = require('koa-static')
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(serve(__dirname + '/build'))
+// app.use(serve(__dirname + '/build'))
+
+app.use(ctx => {
+  ctx.body = 'Hello Koa'
+})
 
 app.listen(3001)
 
