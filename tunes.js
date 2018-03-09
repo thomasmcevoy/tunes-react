ca5e1e655b532aa2797869b02535f71fa2613e0d
Thomas McEvoy <tmcevoy213@gmail.com>
2018-03-09 15:25:18 -0500

    test

--- tunes.js
+++ tunes.js
@@ -2,12 +2,20 @@ const serve = require('koa-static')
 const Koa = require('koa')
 const app = new Koa()
 
+// $ GET /package.json
+app.use(serve('.'));
+
+// $ GET /hello.txt
+app.use(serve('/build'));
+
 // app.use(serve(__dirname + '/build'))
 
-app.use(async ctx => {
-  console.log(ctx)
-  console.log(ctx.request)
-  console.log(ctx.response)
-})
+// app.use(async ctx => {
+//   console.log(ctx)
+//   console.log(ctx.request)
+//   console.log(ctx.response)
+// })
 
 app.listen(3001)
+
+console.log('listening on port 3001')
