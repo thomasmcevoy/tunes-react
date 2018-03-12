064dbcafe565afdd9e6a2d86b79c63b34f805c9c
Thomas McEvoy <tmcevoy213@gmail.com>
2018-03-11 22:53:29 -0400

    fix routing problem?

--- tunes.js
+++ tunes.js
@@ -2,14 +2,5 @@ const serve = require('koa-static')
 const Koa = require('koa')
 const app = new Koa()
 
-// This works
-// app.use(ctx => {
-//   ctx.body = 'Hello Koa'
-// })
-
-// This doesn't work
-app.use(serve(__dirname + 'build'))
-
+app.use(serve(__dirname + '/build'))
 app.listen(3001)
-
-console.log('listening on port 3001')
