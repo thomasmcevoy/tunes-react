3f5553f59da2e4f5d33b46532973ffdd004a3b98
Thomas McEvoy <tmcevoy213@gmail.com>
2018-12-15 21:45:05 -0500

    Updates

---
+++ precache-manifest.0922c848e779591d4118fa26fe62a838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "08eaa9726aa4c5196ecf",
+    "url": "/tunes/static/css/main.d2b4564b.chunk.css"
+  },
+  {
+    "revision": "08eaa9726aa4c5196ecf",
+    "url": "/tunes/static/js/main.08eaa972.chunk.js"
+  },
+  {
+    "revision": "34b1181df8481c00359a",
+    "url": "/tunes/static/js/1.34b1181d.chunk.js"
+  },
+  {
+    "revision": "465c3b7d3f5b68fb596b",
+    "url": "/tunes/static/js/runtime~main.465c3b7d.js"
+  },
+  {
+    "revision": "e338d68a6a434653ba9b02c2c3ee0f57",
+    "url": "/tunes/static/media/Berthold Akzidenz Grotesk BE Medium.e338d68a.ttf"
+  },
+  {
+    "revision": "30546242434470023e9d8df31af6d194",
+    "url": "/tunes/static/media/Berthold Akzidenz Grotesk BE Regular.30546242.ttf"
+  },
+  {
+    "revision": "b71e18267b3e9abc62cd275414121b53",
+    "url": "/tunes/index.html"
+  }
+];
