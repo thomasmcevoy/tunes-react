d46e28e3632ffd3d7df7992f87bdb3018a070624
Thomas McEvoy <thomas@Thomass-MacBook-Pro.local>
2019-02-18 18:38:51 -0500

    updates

--- build/precache-manifest.cd297640aaaa25a01f933645202247e6.js
+++ build/precache-manifest.cd297640aaaa25a01f933645202247e6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "08eaa9726aa4c5196ecf",
+    "revision": "82d7556341ea58a26e72",
     "url": "/tunes/static/css/main.d2b4564b.chunk.css"
   },
   {
-    "revision": "08eaa9726aa4c5196ecf",
-    "url": "/tunes/static/js/main.08eaa972.chunk.js"
+    "revision": "82d7556341ea58a26e72",
+    "url": "/tunes/static/js/main.82d75563.chunk.js"
   },
   {
     "revision": "34b1181df8481c00359a",
@@ -16,15 +16,15 @@ self.__precacheManifest = [
     "url": "/tunes/static/js/runtime~main.465c3b7d.js"
   },
   {
-    "revision": "e338d68a6a434653ba9b02c2c3ee0f57",
-    "url": "/tunes/static/media/Berthold Akzidenz Grotesk BE Medium.e338d68a.ttf"
-  },
-  {
     "revision": "30546242434470023e9d8df31af6d194",
     "url": "/tunes/static/media/Berthold Akzidenz Grotesk BE Regular.30546242.ttf"
   },
   {
-    "revision": "b71e18267b3e9abc62cd275414121b53",
+    "revision": "e338d68a6a434653ba9b02c2c3ee0f57",
+    "url": "/tunes/static/media/Berthold Akzidenz Grotesk BE Medium.e338d68a.ttf"
+  },
+  {
+    "revision": "1b0cba786db3cabe20d66576d7fc7630",
     "url": "/tunes/index.html"
   }
 ];
